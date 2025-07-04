a8573088731cc8bb4b7f15c02e2ac5d4264e583e
igorz <ihorzviahin@gmail.com>
2025-07-04 12:50:26 +0200

    Lesson3_form_useRef

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, {useRef} from 'react';
 
 const App = () => {
+
+    const name = useRef();
+    const age = useRef();
+
+
+    const save = (e) => {
+        e.preventDefault();
+        const user = {
+            name: name.current.value,
+            age: age.current.value
+        };
+        console.log(user)
+    }
+
+
     return (
         <div>
-            app
+            <form>
+                <input type="text" placeholder="name" ref={name}/>
+                <input type="text" placeholder="age" ref={age}/>
+                <button onClick={save}> save</button>
+            </form>
         </div>
     );
 };
