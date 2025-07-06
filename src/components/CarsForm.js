57394bbf8e3cb48a6611d8177e5a9e657684ea4f
igorz <ihorzviahin@gmail.com>
2025-07-06 21:15:03 +0200

    Lesson3_form_HW3+Joivalidator

--- src/components/CarsForm.js
+++ src/components/CarsForm.js
@@ -1,11 +1,15 @@
 import React, {useEffect} from 'react';
 import {useForm} from "react-hook-form";
+import {joiResolver} from "@hookform/resolvers/joi";
+
 import {carService} from "../services/carService";
+import {carValidator} from "../validators/carValidator";
 
 const CarsForm = ({setTrigger, setCarForUpdate, carForUpdate}) => {
 
     const {reset, register, handleSubmit, formState: {isValid, errors}, setValue} = useForm({
         mode: "all",
+        resolver: joiResolver(carValidator)
     });
 
     useEffect(() => {
@@ -22,7 +26,7 @@ const CarsForm = ({setTrigger, setCarForUpdate, carForUpdate}) => {
     const save = async (car) => {
         console.log(car)
         if (carForUpdate) {
-            await  carService.updateById(carForUpdate.id, car)
+            await carService.updateById(carForUpdate.id, car)
             setTrigger(prev => !prev);
         } else {
             await carService.create(car);
@@ -34,28 +38,9 @@ const CarsForm = ({setTrigger, setCarForUpdate, carForUpdate}) => {
 
     return (
         <form onSubmit={handleSubmit(save)}>
-            <input type="text" placeholder={"brand"} {...register("brand", {
-                pattern: {
-                    value: /^[a-zA-Zа-яА-яёЁіІїЇєЄҐґ]{1,20}$/,
-                    message: "bla_bla"
-                }
-            })}/>
-            <input type="text" placeholder={"price"} {...register("price", {
-                valueAsNumber: true,
-                min: {
-                    value: 0,
-                    message: "min 0"
-                },
-                max: {
-                    value: 1000000,
-                    message: "max 1_000_000"
-                },
-            })}/>
-            <input type="text" placeholder={"year"} {...register("year", {
-                valueAsNumber: true,
-                min: {value: 1990, message: "year min 1990"},
-                max: {value: new Date().getFullYear(), message: "max current year"},
-            })}/>
+            <input type="text" placeholder={"brand"} {...register("brand",)}/>
+            <input type="text" placeholder={"price"} {...register("price", {valueAsNumber: true})}/>
+            <input type="text" placeholder={"year"} {...register("year", {valueAsNumber: true})}/>
             <button disabled={!isValid}>save</button>
             {errors.brand && <div>{errors.brand.message}</div>}
             {errors.price && <div>{errors.price.message}</div>}
